c389c8572c125facd23a0240c5d137c42dab26e1
David Bodow <dbodow@gmail.com>
2018-01-22 11:41:57 -0800

    Add __pycache__ to gitignore and removed from tracking

---
+++ machine-learning-hw/w1c1/features/gradient_descent.feature
@@ -0,0 +1,33 @@
+Feature: Use gradient descent to numerically estimate an OLS model
+
+  Scenario: The observed dataset closely fits Y = alpha + beta X
+    Given alpha is -17.5 and beta is 4
+    and noise stddev is 5 for 100 datapoints
+    and learning rate A of 0.0075; learning rate B of 0.0003
+    when the gradient descent is run
+    then the result should converge
+    and observed A should be within a .99 CI for A
+    and observed B should be within a .99 CI for B
+
+  Scenario: The observed dataset loosely fits Y = alpha + beta X
+    Given alpha is -17.5 and beta is 4
+    and noise stddev is 25 for 100 datapoints
+    and learning rate A of 0.0075; learning rate B of 0.0003
+    when the gradient descent is run
+    then the result should converge
+    and observed A should be within a .99 CI for A
+    and observed B should be within a .99 CI for B
+
+  Scenario: The learning rate is too high for A
+    Given alpha is -17.5 and beta is 4
+    and noise stddev is 25 for 100 datapoints
+    and learning rate A of 20; learning rate B of 0.015
+    when the gradient descent is run
+    then the result should diverge with an OverflowError
+
+  Scenario: The learning rate is too high for B
+    Given alpha is -17.5 and beta is 4
+    and noise stddev is 25 for 100 datapoints
+    and learning rate A of 0.01; learning rate B of 20
+    when the gradient descent is run
+    then the result should diverge with an OverflowError
---
+++ machine-learning-hw/w1c1/features/steps/gradient_descent_steps.py
@@ -0,0 +1,77 @@
+from behave import *
+from scipy.stats import norm
+
+import gradient_descent as gd
+import noisy_2d_dataset as ds
+import mse
+
+@given('alpha is {alpha} and beta is {beta}')
+def step_impl(ctx, alpha, beta):
+    ctx.alpha = float(alpha)
+    ctx.beta = float(beta)
+    ctx.true_model = lambda x: ctx.alpha + ctx.beta * x
+
+@given('noise stddev is {stddev} for {num_points} datapoints')
+def step_impl(ctx, stddev, num_points):
+    ctx.stddev = float(stddev)
+    ctx.num_points = int(num_points)
+    ctx.dataset = ds.noisy_2d_dataset(ctx.true_model,\
+        ctx.stddev, ctx.num_points)
+
+@given('learning rate A of {lambda_A}; learning rate B of {lambda_B}')
+def step_impl(ctx, lambda_A, lambda_B):
+    ctx.learning_rates = (float(lambda_A), float(lambda_B))
+
+@when('the gradient descent is run')
+def step_impl(ctx):
+    try:
+        ctx.A_obs, ctx.B_obs =\
+            gd.gradient_descent(ctx.dataset, ctx.learning_rates)
+        ctx.overflow = False
+    except OverflowError:
+        ctx.overflow = True
+
+@then('the result should converge')
+def step_impl(ctx):
+    msg = 'the model diverged with an OverflowError'
+    assert ctx.overflow is False, msg
+
+# Simple reference: https://onlinecourses.science.psu.edu/stat414/node/280
+# Main idea: A ~ N(alpha, variance / n)
+@then('observed A should be within a {tolerance} CI for A')
+def step_impl(ctx, tolerance):
+    # Set critical value
+    z_star = norm.ppf(float(tolerance))
+
+    # Check A is within an acceptable range
+    A_range = z_star * ctx.stddev / (ctx.num_points ** 0.5)
+    A_min = ctx.alpha - A_range
+    A_max = ctx.alpha + A_range
+
+    # Run test
+    msg = f'expected A to be within {A_range} of {ctx.alpha}\
+        got A is {ctx.A_obs}'
+    assert A_min < ctx.A_obs < A_max, msg
+
+# Main idea: B ~ N(beta, variance / SSE)
+@then('observed B should be within a {tolerance} CI for B')
+def step_impl(ctx, tolerance):
+    # Set critical value, SSE
+    z_star = norm.ppf(float(tolerance))
+    sse = len(ctx.dataset) * mse.mse(ctx.true_model, ctx.dataset)
+
+    # Check B is within an acceptable range
+    B_range = z_star * ctx.stddev / (sse ** 0.5)
+    B_min = ctx.beta - B_range
+    B_max = ctx.beta + B_range
+
+    # Run test
+    msg = f'expected B to be within {B_range} of {ctx.beta}\
+        got B is {ctx.B_obs}'
+    assert B_min < ctx.B_obs < B_max, msg
+
+
+@then('the result should diverge with an OverflowError')
+def step_impl(ctx):
+    msg = 'expected an OverflowError, but it was not thrown'
+    assert ctx.overflow is True, msg
