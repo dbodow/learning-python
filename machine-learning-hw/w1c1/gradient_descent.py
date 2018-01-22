58c1865700cd8dd960a0fe00b574829aabbaa241
David Bodow <dbodow@gmail.com>
2018-01-22 13:08:37 -0800

    Add zipped version

--- machine-learning-hw/w1c1/gradient_descent.py
+++ machine-learning-hw/w1c1/gradient_descent.py
@@ -1,6 +1,5 @@
 from numpy import sum, array as np_sum, array
 import mse
-import pdb
 
 """
 gradient_descent: list_of_pairs_of_floats, pair => pair
@@ -8,6 +7,8 @@ gradient_descent: list_of_pairs_of_floats, pair => pair
 given an observed dataset of (x, y) pairs,
 and a pair of learning rate parameters (for A and B respectively),
 uses gradient descent to estimate a linear model's alpha and beta
+
+NOTE: uses the convention Y = alpha + beta X for a linear model
 """
 
 def gradient_descent(dataset, learning_rates):
@@ -16,22 +17,24 @@ def gradient_descent(dataset, learning_rates):
     num_points = len(dataset)
 
     errors = lambda a, b: np_sum(ys - a - b * xs)
-    dMSE_dA = lambda a, b: (-2 / num_points) * sum(errors(a, b))
-    dMSE_dB = lambda a, b: (-2 / num_points) * sum(xs * errors(a, b))
+    dMSE_dA = lambda a, b, errs: (-2 / num_points) * sum(errs)
+    dMSE_dB = lambda a, b, errs: (-2 / num_points) * sum(xs * errs)
     report_epoch = lambda epoch, A, B, MSE: print(f"{epoch}, {A}, {B}, {MSE}")
 
-    A, B = -20, 3
+    A, B = 0.0, 0.0
+    # A, B = -20.0, -3.5 # Alternative start closer to test suite
     past_MSE = float('inf')
     print("EPOCH  |  A  |  B  |  MSE")
     print("-------------------------")
     for epoch in range(300):
-        dA = dMSE_dA(A, B)
-        dB = dMSE_dB(A, B)
+        current_errors = errors(A, B)
+        dA = dMSE_dA(A, B, current_errors)
+        dB = dMSE_dB(A, B, current_errors)
 
         A -= lambda_A * dA
         B -= lambda_B * dB
         model = lambda x: A + B*x
-        # pdb.set_trace()
+
         current_MSE = mse.mse(model, dataset)
         if epoch % 10 == 0:
             print(epoch, round(A, 2), round(B, 2), round(current_MSE, 2))
